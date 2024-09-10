45d1f9e5af71129f4d6eeed57fac81842db96f8d
Sowndharya617 <sowndharya617@gmail.com>
2024-09-10 06:02:57 +0530

    Found out lastdigit of a number

---
+++ lastdigit.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class lastdigit{
+    public static void main(String args[]){
+        Scanner sow = new Scanner(System.in);
+        int a = sow.nextInt();
+        int b=a%10;
+        System.out.println(b); 
+    }
+}
