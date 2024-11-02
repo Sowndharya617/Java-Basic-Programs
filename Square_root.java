f9b0d3de53e4f518c7b83b56e8b17e80152d9163
Sowndharya A <144271999+Sowndharya617@users.noreply.github.com>
2024-11-02 23:35:33 +0530

    Create Square_root.java

---
+++ Square_root.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class square{
+    public static void main(String[]args){
+        Scanner sow = new Scanner(System.in);
+        int a = sow.nextInt();
+        double res = Math.sqrt(a);
+        System.out.print(res);
+    }
+}
